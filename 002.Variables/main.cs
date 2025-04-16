0f04b757ed298c77d4bdbf5fcdbeb21b33ec15cd
Mahan Mehrabi <gnusmhn@gmail.com>
2025-04-16 16:52:23 +0330

    Add files via upload

---
+++ 002.Variables/main.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication90
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //variables = they can store thing for you
+            //in c# when we want to define a variable we have to write the type of it before their name
+            string text = "Hello World!"; //String variables can hold texts inside in them
+            int num1 = 100; //integer variables can hold whole numbers
+            double num2 = 176.54; //double variables can hold numbers whit decimal points
+        }
+    }
+}
